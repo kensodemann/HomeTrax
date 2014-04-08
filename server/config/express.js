38cc6b4efaa12cd420cc70f87e8cc0e3ac2d3419
Ken Sodemann <ken.sodemann@gmail.com>
2014-04-08 14:39:08 -0500

    Copy some of the setup from the training module.

--- server/config/express.js
+++ server/config/express.js
@@ -1,6 +1,27 @@
+var express = require('express');
+
 module.exports = function(app, config) {
   app.configure(function() {
     app.set('view engine', 'jade');
     app.set('views', config.rootPath + '/server/views');
+
+    // app.use(stylus.middleware({
+    //   src: config.rootPath + '/public',
+    //   compile: compile
+    // }));
+
+    app.use(express.logger('dev'));
+    app.use(express.cookieParser());
+    app.use(express.json());
+    app.use(express.urlencoded());
+    
+    // app.use(express.session({
+    //   secret: 'multi vision unicorns'
+    // }));
+    
+    // app.use(passport.initialize());
+    // app.use(passport.session());
+
+    app.use(express.static(config.rootPath + '/public'));
   });
 }
