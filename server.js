68be7de29cb3422af85de1859f88257dc7608867
Ken Sodemann <ken.sodemann@gmail.com>
2014-04-07 19:12:29 -0500

    Upgrade version of express.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 #!/bin/env node
 //  OpenShift sample Node application
 var express = require('express');
-var fs      = require('fs');
+var fs = require('fs');
 
 
 /**
@@ -113,7 +113,7 @@ var SampleApp = function() {
      */
     self.initializeServer = function() {
         self.createRoutes();
-        self.app = express.createServer();
+        self.app = express();
 
         //  Add handlers for the app (from the routes).
         for (var r in self.routes) {
