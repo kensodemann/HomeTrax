c81f920738db63bb0cb23c207e8d4f0e1590b4ec
Ken Sodemann <ken.sodemann@gmail.com>
2014-04-08 11:12:16 -0500

    Test Address and Port setup.

--- test/server/appSpec.js
+++ test/server/appSpec.js
@@ -10,4 +10,38 @@ describe('ServerApp', function() {
   it('Builds', function() {
     chai.expect(app).to.not.be.null;
   });
+
+  describe('Server and Port setup', function() {
+    var previousEnv;
+
+    beforeEach(function() {
+      previousEnv = process.env;
+    });
+
+    afterEach(function() {
+      process.env = previousEnv;
+    });
+
+    it('Defaults to localhost:8080', function() {
+      process.env = {};
+
+      app.setupVariables();
+
+      chai.expect(app.ipaddress).to.equal('127.0.0.1');
+      chai.expect(app.port).to.equal(8080);
+    });
+
+    it('Uses OPENSHIFT parameters if specified', function() {
+      process.env = {
+        OPENSHIFT_NODEJS_IP: '1.2.3.4',
+        OPENSHIFT_NODEJS_PORT: 3030
+      };
+
+      app.setupVariables();
+
+      chai.expect(app.ipaddress).to.equal('1.2.3.4');
+      chai.expect(app.port).to.equal(3030);
+    });
+
+  });
 });
