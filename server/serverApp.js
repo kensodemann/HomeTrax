9df45e6327102cff0b3b69c9f28d712b15586cea
Ken Sodemann <ken.sodemann@gmail.com>
2014-04-12 12:21:09 -0500

    Move database config

--- server/serverApp.js
+++ server/serverApp.js
@@ -21,21 +21,8 @@ var ServerApp = function() {
   };
 
   self.setupConnectString = function() {
-    if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-      self.connectString = openShiftConnectString(process.env);
-    } else {
-      self.connectString = '127.0.0.1:27017/HomeApp';
-    }
+    self.connectString = require('./config/database').connectString(process.env);
   };
-
-  function openShiftConnectString(env) {
-    return env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-      env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-      env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-      env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-      env.OPENSHIFT_APP_NAME;
-  }
-
 
   /*
    * Walking Skel code that is just used to get the mongo message
