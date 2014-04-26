cd8b4eea7fbcb727357208dc3e4813fa8e90b0ac
Ken Sodemann <ken.sodemann@gmail.com>
2014-04-26 15:48:24 -0500

    Test login redirection.

--- test/client/account/trxLoginCtrlSpec.js
+++ test/client/account/trxLoginCtrlSpec.js
@@ -23,7 +23,7 @@ describe('trxLoginCtrl', function() {
       mockAuth.authenticateUser.returns(dfd.promise);
     }));
 
-    it('It should call authenticateUser', function() {
+    it('Should call authenticateUser', function() {
       var ctrl = $controllerConstructor('trxLoginCtrl', {
         $scope: scope,
         $location: {},
@@ -33,5 +33,25 @@ describe('trxLoginCtrl', function() {
 
       expect(mockAuth.authenticateUser.calledWith('jeff', 'FireW00d')).to.be.true;
     });
+
+    it('Should redirect to index on success', function() {
+      var mockLocation = sinon.stub({
+        path: function() {},
+        replace: function() {}
+      });
+      mockLocation.path.returns(mockLocation);
+
+      var ctrl = $controllerConstructor('trxLoginCtrl', {
+        $scope: scope,
+        $location: mockLocation,
+        trxAuthentication: mockAuth
+      });
+      scope.signin('jeff', 'FireW00d');
+      dfd.resolve(true);
+      scope.$digest();
+
+      expect(mockLocation.path.calledWith('/')).to.be.true;
+      expect(mockLocation.replace.calledOnce).to.be.true;
+    })
   });
 })
