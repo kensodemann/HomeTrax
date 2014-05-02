093b1a79017de4a2a88552fa27628b6eeecc6322
Ken Sodemann <ken.sodemann@gmail.com>
2014-05-02 10:00:53 -0500

    test the index.

--- test/server/config/routesSpec.js
+++ test/server/config/routesSpec.js
@@ -1,25 +1,38 @@
 var expect = require('chai').expect;
 var express = require('express');
+var path = require('path');
 var request = require('supertest');
-var config = require('../../../server/config/config');
 
 describe('routes', function() {
   var app;
 
   beforeEach(function() {
     app = express();
+    var fakeViewPath = path.normalize(__dirname + '/../mockViews');
     app.set('view engine', 'jade');
-    app.set('views', config.rootPath + '/server/views');
+    app.set('views', fakeViewPath);
     require('../../../server/config/routes')(app);
   });
 
-  it('at least works', function(done) {
-    // TODO: This is the basic idea, but let's get rid of the use of jade
-    //       and let's create our own test views that just have the name
-    //       of the view in them.
+  it('Goes to the inedex view for the root route', function(done) {
     request(app)
       .get('/')
+      .expect('<index></index>')
       .expect(200, done);
   });
+
+  it('Goes to the inedex view for unkown routes', function(done) {
+    request(app)
+      .get('/i_dont_exist')
+      .expect('<index></index>')
+      .expect(200, done);
+  });
+
+  // it('Loads valid partials', function(done) {
+  //   request(app)
+  //     .get('/partials/calendar/main')
+  //     .expect('<index></index>')
+  //     .expect(200, done);
+  // });
 
 });
