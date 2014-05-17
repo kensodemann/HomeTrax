c8ff46d30ed0e855d36379a3a62460909c56759a
Ken Sodemann <ken.sodemann@gmail.com>
2014-05-17 10:24:23 -0500

    Basic structure in place.

--- public/app/account/trxUserListCtrl.js
+++ public/app/account/trxUserListCtrl.js
@@ -2,5 +2,13 @@ angular.module('app')
   .controller('trxUserListCtrl', ['$scope', 'trxUser',
     function($scope, trxUser) {
       $scope.users = trxUser.query();
+
+      $scope.update = function(user) {
+        var myScope = $scope;
+      };
+
+      $scope.create = function() {
+        console.log("Well ain't that this shit?  I am not done yet.");
+      };
     }
   ])
