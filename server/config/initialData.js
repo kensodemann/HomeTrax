0bfb1632ff11f9c697cc87fb46d746fa3b2b0c50
Ken Sodemann <ken.sodemann@gmail.com>
2014-05-22 11:49:53 -0500

    Create default admin account.

--- server/config/initialData.js
+++ server/config/initialData.js
@@ -1,36 +1,26 @@
 var db = require('./database');
 var encryption = require('../services/encryption');
 
-// NOTE: this is mostly just for use in development.  Some of this may stay,
-//       but most of it should be removed before the first real realease.
-
-function createDefaultUsers() {
-  db.users.find(function(err, users) {
+function createDefaultAdministrator() {
+  db.users.find({
+    isDefaultAdmin: true
+  }, function(err, users) {
     if (users.length === 0) {
       var salt = encryption.createSalt();
-      var hash = encryption.hash(salt, 'ken');
+      var hash = encryption.hash(salt, 'the default admin password');
       db.users.save({
-        firstName: 'Ken',
-        lastName: 'Sodemann',
-        username: 'ken',
+        firstName: 'Default',
+        lastName: 'Administrator',
+        username: 'admin',
         salt: salt,
         hashedPassword: hash,
+        isDefaultAdmin: true,
         roles: ['admin']
-      });
-      salt = encryption.createSalt();
-      hash = encryption.hash(salt, 'lisa');
-      db.users.save({
-        firstName: 'Lisa',
-        lastName: 'Sodemann',
-        username: 'lisa',
-        salt: salt,
-        hashedPassword: hash,
-        roles: []
       });
     }
   });
-};
+}
 
 module.exports = function() {
-  createDefaultUsers();
+  createDefaultAdministrator();
 }
