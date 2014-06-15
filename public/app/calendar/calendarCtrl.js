a80fefa842014b9b6d9fa8fc90b3dbf4ac3adcbc
Ken Sodemann <ken.sodemann@gmail.com>
2014-06-15 07:30:43 -0500

    Move in-place editing code.

--- public/app/calendar/calendarCtrl.js
+++ public/app/calendar/calendarCtrl.js
@@ -1,6 +1,3 @@
-// In combination with this: http://www.bennadel.com/blog/2450-using-ngcontroller-with-ngrepeat-in-angularjs.htm
-// This could be used to do an editable table (I am thinking aobut for the account transactions, etc.)
-// TODO: Move this to the account page before adding the calendar to this page.
 angular.module('app')
   .controller('calendarCtrl', function($scope) {
     $scope.message = "Hello World from the Angular Calendar!!";
