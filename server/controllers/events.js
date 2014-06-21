4bb616cc477150aee2cd00148659485289619238
Ken Sodemann <ken.sodemann@gmail.com>
2014-06-21 16:37:43 -0500

    Create getById routine.

--- server/controllers/events.js
+++ server/controllers/events.js
@@ -1,3 +1,5 @@
+'use strict'
+
 var db = require('../config/database');
 var ObjectId = require("mongojs").ObjectId;
 
@@ -10,5 +12,22 @@ module.exports.get = function(req, res) {
     }]
   }, function(err, events) {
     res.send(events);
+  });
+};
+
+module.exports.getById = function(req, res) {
+  db.events.findOne({
+    _id: ObjectId(req.params.id)
+  }, function(err, ev) {
+    if (ev) {
+    	if (ev.private && ev.userId.toString() !== req.user._id.toString()){
+    	 	res.status(403);
+    	 	return res.send();
+    	}
+      res.send(ev);
+    } else {
+      res.status(404);
+      res.send();
+    }
   });
 };
--- test/server/controllers/eventsSpec.js
+++ test/server/controllers/eventsSpec.js
@@ -7,6 +7,11 @@ var db = require('../../../server/config/database');
 var ObjectId = require('mongojs').ObjectId;
 
 describe('events controller', function() {
+  var myPublicEvent;
+  var myPrivateEvent;
+  var otherUserPublicEvent;
+  var otherUserPrivateEvent;
+
   beforeEach(function(done) {
     loadEvents(done);
   });
@@ -29,64 +34,132 @@ describe('events controller', function() {
     });
 
     it('returns events for user and non-private events for other users', function(done) {
-        eventsController.get(req, {
-          send: function(events) {
-            expect(events.length).to.equal(3);
-            events.forEach(function(e, idx) {
-              expect(e.private).to.be.a('Boolean');
-              if (e.private) {
-                expect(e.userId.toString()).to.equal('53a4dd887c6dc30000bee3af');
-              }
-            });
-            done();
-          }
-        });
+      eventsController.get(req, {
+        send: function(events) {
+          expect(events.length).to.equal(3);
+          events.forEach(function(e, idx) {
+            expect(e.private).to.be.a('Boolean');
+            if (e.private) {
+              expect(e.userId.toString()).to.equal('53a4dd887c6dc30000bee3af');
+            }
+          });
+          done();
+        }
       });
+    });
   });
-});
 
-function loadEvents(done) {
-  db.events.remove({}, function() {
-    db.events.save({
-      title: 'Eat Something',
-      allDay: false,
-      start: '2014-06-20T12:00:00',
-      end: '2014-06-20T13:00:00',
-      category: 'Health & Fitness',
-      private: false,
-      userId: ObjectId('53a4dd887c6dc30000bee3af')
-    }, function() {
+  describe('getById', function() {
+    var req;
+
+    beforeEach(function() {
+      req = sinon.stub({
+        user: {
+          _id: '53a4dd887c6dc30000bee3af'
+        },
+        params: {}
+      });
+    });
+
+    it('returns a status of 404 if the event does not exist', function(done) {
+      var status;
+      req.params.id = '53a4dd887c6dc30000bee3af';
+      eventsController.getById(req, {
+        status: function(s) {
+          status = s;
+        },
+        send: function(e) {
+          expect(e).to.be.undefined;
+          expect(status).to.equal(404);
+          done();
+        }
+      });
+    });
+
+    it('returns the event if the event exists and is mine', function(done) {
+      req.params.id = myPrivateEvent._id.toString();
+      eventsController.getById(req, {
+        send: function(e) {
+          expect(e).to.deep.equal(myPrivateEvent);
+          done();
+        }
+      });
+    });
+
+    it('returns the event if the event exists, is not mine, but is public', function(done) {
+      req.params.id = otherUserPublicEvent._id.toString();
+      eventsController.getById(req, {
+        send: function(e) {
+          expect(e).to.deep.equal(otherUserPublicEvent);
+          done();
+        }
+      });
+    });
+
+    it('returns a status of 403 if the event exists, is not mine and is private', function(done) {
+      var status;
+      req.params.id = otherUserPrivateEvent._id.toString();
+      eventsController.getById(req, {
+        status: function(s){
+          status = s;
+        },
+        send: function(e) {
+          expect(e).to.be.undefined;
+          expect(status).to.equal(403);
+          done();
+        }
+      });
+    });
+
+  });
+
+  function loadEvents(done) {
+    db.events.remove({}, function() {
       db.events.save({
-        title: 'Fart',
+        title: 'Eat Something',
         allDay: false,
-        start: '2014-06-20T13:01:00',
-        end: '2014-06-20T13:05:00',
+        start: '2014-06-20T12:00:00',
+        end: '2014-06-20T13:00:00',
         category: 'Health & Fitness',
-        private: true,
+        private: false,
         userId: ObjectId('53a4dd887c6dc30000bee3af')
-      }, function() {
+      }, function(error, value) {
+        myPublicEvent = value;
         db.events.save({
-          title: 'Have Sex',
+          title: 'Fart',
           allDay: false,
-          start: '2014-06-22T16:00:00',
-          end: '2014-06-22T18:45:00',
-          category: 'Recreation',
-          private: false,
-          userId: ObjectId('53a4dd887c6dc30000bee3ae')
-        }, function() {
+          start: '2014-06-20T13:01:00',
+          end: '2014-06-20T13:05:00',
+          category: 'Health & Fitness',
+          private: true,
+          userId: ObjectId('53a4dd887c6dc30000bee3af')
+        }, function(error, value) {
+          myPrivateEvent = value;
           db.events.save({
-            title: 'Sleep',
+            title: 'Have Sex',
             allDay: false,
-            start: '2014-06-23T12:00:00',
-            end: '2014-06-20T13:00:00',
-            category: 'Health & Fitness',
-            private: true,
+            start: '2014-06-22T16:00:00',
+            end: '2014-06-22T18:45:00',
+            category: 'Recreation',
+            private: false,
             userId: ObjectId('53a4dd887c6dc30000bee3ae')
-          }, function() {
-            done();
+          }, function(error, value) {
+            otherUserPublicEvent = value
+            db.events.save({
+              title: 'Sleep',
+              allDay: false,
+              start: '2014-06-23T12:00:00',
+              end: '2014-06-20T13:00:00',
+              category: 'Health & Fitness',
+              private: true,
+              userId: ObjectId('53a4dd887c6dc30000bee3ae')
+            }, function(error, value) {
+              otherUserPrivateEvent = value;
+              done();
+            });
           });
         });
       });
     });
-  });
-}
+  }
+});
