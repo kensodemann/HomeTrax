2ba4f6e55790de3f63e510f07317dff0fe4733bc
Ken Sodemann <ken.sodemann@gmail.com>
2014-07-10 21:16:06 -0500

    Bring in the ui.bootstrap directive.

--- public/app/app.js
+++ public/app/app.js
@@ -1,4 +1,4 @@
-angular.module('app', ['ngRoute', 'ngResource', 'ui.calendar']);
+angular.module('app', ['ngRoute', 'ngResource', 'ui.calendar', 'ui.bootstrap']);
 
 angular.module('app')
   .config(function($routeProvider, $locationProvider) {
