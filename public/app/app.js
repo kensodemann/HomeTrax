aee12e8307ba5edceb97a973f41d0ab4470a2c45
Ken Sodemann <ken.sodemann@gmail.com>
2014-08-02 08:33:07 -0500

    Remove Bootstrap Datepicker.

--- public/app/app.js
+++ public/app/app.js
@@ -1,4 +1,4 @@
-angular.module('app', ['ngRoute', 'ngResource', 'ui.calendar', 'ui.bootstrap', 'ui.bootstrap.datetimepicker']);
+angular.module('app', ['ngRoute', 'ngResource', 'ui.calendar', 'ui.bootstrap']);
 
 angular.module('app')
   .config(function($routeProvider, $locationProvider) {
