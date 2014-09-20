596f7ac7cf9bfb51452bbd3ac872e2a638f088ce
Ken Sodemann <ken.sodemann@gmail.com>
2014-09-20 08:23:11 -0500

    Add Only My Events flag

--- public/app/calendar/calendarCtrl.js
+++ public/app/calendar/calendarCtrl.js
@@ -1,30 +1,34 @@
 angular.module('app')
   .controller('calendarCtrl', ['$scope', '$modal', 'calendarEvent', 'eventCategory',
-    function ($scope, $modal, calendarEvent, eventCategory){
-      $scope.dayClicked = function (day){
+    function($scope, $modal, calendarEvent, eventCategory) {
+      $scope.events = calendarEvent.query(function(events) {
+        $scope.eventSources[0].events = events.slice(0);
+      });
+
+      $scope.dayClicked = function(day) {
         var event = new calendarEvent();
-        event.start = new moment(day.hour(8));
-        event.end = new moment(day.hour(9));
+        event.start = moment(day.hour(8));
+        event.end = moment(day.hour(9));
         event.allDay = false;
         var m = openModal(event);
-        m.result.then(function (e){
+        m.result.then(function(e) {
           $scope.events.push(e);
         })
       };
 
-      $scope.eventClicked = function (event){
+      $scope.eventClicked = function(event) {
         var m = openModal(event);
 
-        m.result.then(function (evt){
+        m.result.then(function(evt) {
           if (typeof evt === 'object') {
             replaceEvent(evt);
-          }else{
+          } else {
             removeEvent(event);
           }
           $scope.calendar.fullCalendar('render');
         });
 
-        function replaceEvent(evt){
+        function replaceEvent(evt) {
           var idx = firstMatchingIndex(evt);
           if (shouldRemoveFromCalendar($scope.events[idx], evt)) {
             $scope.calendar.fullCalendar('removeEvents', evt._id);
@@ -33,27 +37,27 @@ angular.module('app')
           $scope.events.push(evt);
         }
 
-        function removeEvent(evt){
+        function removeEvent(evt) {
           var idx = firstMatchingIndex(evt);
           $scope.events.splice(idx, 1);
           $scope.calendar.fullCalendar('removeEvents', evt._id);
         }
 
-        function firstMatchingIndex(evt){
-          var matchingEvts = $.grep($scope.events, function (e){
+        function firstMatchingIndex(evt) {
+          var matchingEvts = $.grep($scope.events, function(e) {
             return e._id === evt._id;
           });
           return $.inArray(matchingEvts[0], $scope.events);
         }
 
-        function shouldRemoveFromCalendar(originalEvent, editedEvent){
+        function shouldRemoveFromCalendar(originalEvent, editedEvent) {
           return (originalEvent.allDay && !editedEvent.allDay) ||
             (!originalEvent.allDay && editedEvent.allDay) ||
             (originalEvent.title !== editedEvent.title);
         }
       };
 
-      $scope.eventDropped = function (event){
+      $scope.eventDropped = function(event) {
         console.log('You dropped the bomb on me');
       };
 
@@ -72,26 +76,52 @@ angular.module('app')
         }
       };
 
-      $scope.events = calendarEvent.query();
-
       $scope.eventSources = [
         {
-          events: $scope.events
+          events: []//$scope.events.slice(0)
         }
       ];
 
+      $scope.showOnlyMine = false;
 
-      function openModal(model){
+      $scope.$watch('showOnlyMine', function(onlyMine) {
+        // So, here is what I think needs to be done:
+        //   On show:
+        //      Create list of items that are not mine
+        //      Push those items
+        //   On hide:
+        //      Create list of items that are not mine
+        //      Remove those items
+        var otherEvents = $.grep($scope.events, function(e) {
+          return e.title !== 'New All Day Event' && e.title !== 'Sad Day';
+        });
+
+        if (onlyMine) {
+          angular.forEach(otherEvents, function(evt) {
+            $scope.calendar.fullCalendar('removeEvents', evt._id);
+            var idx = $.inArray(evt, $scope.eventSources[0].events);
+            $scope.eventSources[0].events.splice(idx, 1);
+          });
+        } else {
+         angular.forEach(otherEvents, function(evt){
+           $scope.eventSources[0].events.push(evt);
+         });
+        }
+      });
+
+
+      function openModal(model) {
         return $modal.open({
           templateUrl: '/partials/calendar/eventEditor',
           controller: 'eventEditorCtrl',
           backdrop: 'static',
           resolve: {
-            eventModel: function (){
+            eventModel: function() {
               return model;
             }
           }
         });
       }
     }
-  ]);
+  ])
+;
