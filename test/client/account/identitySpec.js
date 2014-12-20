11428c2b59a83aa8ae6118f3ffc6deb2f2f45dcd
Ken Sodemann <ken.sodemann@gmail.com>
2014-12-20 03:46:32 +0000

    fix session user handling

--- test/client/account/identitySpec.js
+++ test/client/account/identitySpec.js
@@ -40,7 +40,7 @@
     }));
 
     describe('instantiation', function() {
-      it('sets the current user', function() {
+      it('sets the current user if there is one', function() {
         expect(mockUserConstructor.calledOnce).to.be.true;
         expect(serviceUnderTest.currentUser.firstName).to.equal('Bill');
         expect(serviceUnderTest.currentUser.lastName).to.equal('Jackson');
@@ -49,26 +49,8 @@
     });
 
     describe('isAuthenticated', function() {
-      beforeEach(function() {
-        mockUserConstructor.returns({});
-      });
-
-      it('resets the current user', function() {
-        mockWindow.bootstrappedUserObject = {
-          _id: 73,
-          firstName: 'Jimm',
-          lastName: 'Smyth',
-          roles: ['admin']
-        };
-        serviceUnderTest.isAuthenticated();
-        expect(mockUserConstructor.calledTwice).to.be.true;
-        expect(serviceUnderTest.currentUser.firstName).to.equal('Jimm');
-        expect(serviceUnderTest.currentUser.lastName).to.equal('Smyth');
-        expect(serviceUnderTest.currentUser.roles).to.deep.equal(['admin']);
-      });
-
       it('returns false if there is no user', function() {
-        mockWindow.bootstrappedUserObject = null;
+        serviceUnderTest.currentUser = undefined;
         expect(serviceUnderTest.isAuthenticated()).to.be.false;
       });
 
@@ -78,17 +60,13 @@
     });
 
     describe('isAuthorized', function() {
-      beforeEach(function() {
-        mockUserConstructor.returns({});
-      });
-
       it('returns false if there is no user', function() {
-        mockWindow.bootstrappedUserObject = null;
+        serviceUnderTest.currentUser = undefined;
         expect(serviceUnderTest.isAuthorized('')).to.be.false;
       });
 
       it('returns false if there is a user, but user does not have role', function() {
-        mockWindow.bootstrappedUserObject = {
+        serviceUnderTest.currentUser = {
           _id: 'something',
           roles: ['user', 'cook']
         };
@@ -96,7 +74,7 @@
       });
 
       it('returns true if there is a user, and user does have role', function() {
-        mockWindow.bootstrappedUserObject = {
+        serviceUnderTest.currentUser = {
           _id: 'something',
           roles: ['user', 'cook', 'admin']
         };
@@ -104,7 +82,7 @@
       });
 
       it('returns true if there is a user, and no role is required', function() {
-        mockWindow.bootstrappedUserObject = {
+        serviceUnderTest.currentUser = {
           _id: 'something',
           roles: ['user', 'cook', 'admin']
         };
@@ -113,14 +91,14 @@
       });
 
       it('returns false if a role is required but the user does not have any roles', function() {
-        mockWindow.bootstrappedUserObject = {
+        serviceUnderTest.currentUser = {
           _id: 'something'
         };
         expect(serviceUnderTest.isAuthorized('admin')).to.be.false;
       });
 
       it('returns true if a role is not required and the user does not have any roles', function() {
-        mockWindow.bootstrappedUserObject = {
+        serviceUnderTest.currentUser = {
           _id: 'something'
         };
         expect(serviceUnderTest.isAuthorized('')).to.be.true;
