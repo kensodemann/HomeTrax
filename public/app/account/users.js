f6bb11d2f92d9c4539e37a3bbac3345093caf971
Ken Sodemann <ken.sodemann@gmail.com>
2014-12-23 20:30:00 +0000

    get specific user from cache

--- public/app/account/users.js
+++ public/app/account/users.js
@@ -1,18 +1,42 @@
 /* global angular */
 (function() {
   'use strict';
-  
+
   angular.module('app.account').factory('users', Users);
 
-  function Users(User) {
-    var exports = {};
-    
+  function Users($q, User) {
+    var exports = {
+      get: getUser
+    };
+
     getAllUsers();
-    
+
     return exports;
-    
-    function getAllUsers(){
-      User.query();
+
+    function getAllUsers() {
+      exports.all = User.query();
+      return exports.all.$promise;
+    }
+
+    function getUser(id) {
+      var dfd = $q.defer();
+      var user = findUser(id);
+      if (!!user) {
+        dfd.resolve(user);
+      }
+      else {
+        getAllUsers().then(function() {
+          dfd.resolve(findUser(id));
+        });
+      }
+      return dfd.promise;
+
+      function findUser(id) {
+        var matching = $.grep(exports.all, function(user) {
+          return user._id === id;
+        });
+        return matching.length > 0 ? matching[0] : undefined;
+      }
     }
   }
 })();
--- test/client/account/usersSpec.js
+++ test/client/account/usersSpec.js
@@ -4,6 +4,9 @@
 
   describe('users', function() {
     var mockUser;
+    var queryUsersDfd;
+    var scope;
+    var testData;
 
     var serviceUnderTest;
 
@@ -12,14 +15,27 @@
     beforeEach(buildMocks);
     beforeEach(provideMocks);
 
-    beforeEach(inject(function(users) {
+    beforeEach(inject(function($rootScope, $q, users) {
+      scope = $rootScope;
       serviceUnderTest = users;
+      queryUsersDfd = $q.defer();
+      testData.$promise = queryUsersDfd.promise;
     }));
 
     function buildMocks() {
+      testData = [{
+        _id: '42',
+        firstName: 'Billy',
+        lastName: 'Bathgate'
+      }, {
+        _id: '73',
+        firstName: 'Owen',
+        lastName: 'Meany'
+      }];
       mockUser = sinon.stub({
         query: function() {}
       });
+      mockUser.query.returns(testData);
     }
 
     function provideMocks() {
@@ -32,9 +48,75 @@
       expect(serviceUnderTest).to.not.be.undefined;
     });
 
-    describe('initiation', function() {
+    describe('instantiation', function() {
       it('queriues all of the users', function() {
         expect(mockUser.query.calledOnce).to.be.true;
+      });
+
+      it('assigns the returned users to "all"', function() {
+        expect(serviceUnderTest.all.length).to.equal(2);
+        expect(serviceUnderTest.all[0]._id).to.equal('42');
+        expect(serviceUnderTest.all[1]._id).to.equal('73');
+      });
+    });
+
+    describe('get', function() {
+      it('resolves to the matching user from cache', function(done) {
+        serviceUnderTest.get('73').then(function(user) {
+          expect(mockUser.query.calledOnce).to.be.true;
+          expect(user._id).to.equal('73');
+          expect(user.firstName).to.equal('Owen');
+          expect(user.lastName).to.equal('Meany');
+          done();
+        });
+        scope.$digest();
+      });
+
+      it('requeries the users if the user does not exist', function(done) {
+        serviceUnderTest.get('71').then(function() {
+          expect(mockUser.query.calledTwice).to.be.true;
+          done();
+        });
+        queryUsersDfd.resolve();
+        scope.$digest();
+      });
+
+      it('resolves to the matching user after requery', function(done) {
+        var newTestData = [{
+          _id: '42',
+          firstName: 'Billy',
+          lastName: 'Bathgate'
+        }, {
+          _id: '73',
+          firstName: 'Owen',
+          lastName: 'Meany'
+        }, {
+          _id: '71',
+          firstName: 'Jimmy',
+          lastName: 'Johns'
+        }];
+        newTestData.$promise = queryUsersDfd.promise;
+        mockUser.query.returns(newTestData);
+        serviceUnderTest.get('71').then(function(user) {
+          expect(mockUser.query.calledTwice).to.be.true;
+          expect(user).to.deep.equal({
+            _id: '71',
+            firstName: 'Jimmy',
+            lastName: 'Johns'
+          });
+          done();
+        });
+        queryUsersDfd.resolve();
+        scope.$digest();
+      });
+
+      it('resolves to undefined if there is no matching user after requery', function(done) {
+        serviceUnderTest.get('71').then(function(user) {
+          expect(user).to.be.undefined;
+          done();
+        });
+        queryUsersDfd.resolve();
+        scope.$digest();
       });
     });
   });
