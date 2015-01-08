8bfd3243a354f6a2510389301ed465989ee5c05a
Ken Sodemann <ken.sodemann@gmail.com>
2015-01-08 19:35:06 +0000

    Create color service

--- public/app/common/colors.js
+++ public/app/common/colors.js
@@ -2,6 +2,22 @@
 (function() {
   'use strict';
 
-  angular.module('app.core').constant('colors', {
-  });
+  angular.module('app.core').factory('colors', ColorService);
+
+  function ColorService() {
+    var exports = {
+      calendar: 0,
+      appointment: 1,
+      task: 2,
+      anniversary: 3,
+
+      getColor: getColor
+    };
+
+    function getColor() {
+      return "#FFFFFF";
+    }
+
+    return exports;
+  }
 })();
