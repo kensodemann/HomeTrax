5697429f77327881a7cb658ba3de97a5dcaa3d5c
Ken Sodemann <ken.sodemann@gmail.com>
2015-01-08 19:28:35 +0000

    Strip out old code

--- test/client/common/colorsSpec.js
+++ test/client/common/colorsSpec.js
@@ -15,24 +15,5 @@
       expect(serviceUnderTest).to.not.be.undefined;
     });
 
-    describe('user colors', function() {
-      it('returns nine colors', function() {
-        var colors = serviceUnderTest.userColors;
-        expect(colors.length).to.equal(9);
-      });
-    });
-
-    describe('event colors', function() {
-      it('contains nine colors', function() {
-        var colors = serviceUnderTest.eventColors;
-        expect(colors.length).to.equal(9);
-      });
-
-      it('does not overlap with the user colors', function() {
-        serviceUnderTest.eventColors.forEach(function(color) {
-          expect(serviceUnderTest.userColors.indexOf(color)).to.equal(-1);
-        });
-      });
-    });
   });
 })();
