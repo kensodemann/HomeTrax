c4e296788eca2f366a6c9f5455e184dbccfb0f86
Ken Sodemann <ken.sodemann@gmail.com>
2015-03-14 17:46:11 -0500

    Add the definition list panel directive.

--- public/app/household/householdCtrl.js
+++ public/app/household/householdCtrl.js
@@ -15,11 +15,11 @@
       state: 'WI',
       postal: '53189',
       phone: '(920) 988-4261',
-      purchaseDate: '2012-10-12T00:00:00',
-      purchasePrice: 176000.00,
-      mortgageBalance: 123432.00,
-      propertyTaxes: 3234.00,
-      insuranceCompany: 'Farmers',
+      purchaseDate: '10/12/2013',
+      purchasePrice: '176,000.00',
+      mortgageBalance: '123,432.00',
+      propertyTaxes: '3,234.00',
+      insuranceCompany: 'Farmer\'s Insurance',
       policyNumber: '1234-45'
     };
 
@@ -29,5 +29,22 @@
     self.addressLines.push(household.addressLine2);
     self.addressLines.push(household.city + ', ' + household.state + ' ' + household.postal);
     self.addressLines.push(household.phone);
+
+    function InfoItem(label, value){
+      this.label = label + ':';
+      this.value = value;
+    }
+
+    var financialData = [];
+    var insuranceData = [];
+    financialData.push(new InfoItem('Purchase Date', household.purchaseDate));
+    financialData.push(new InfoItem('Purchase Price', household.purchasePrice));
+    financialData.push(new InfoItem('Mortgage Balance', household.mortgageBalance));
+    financialData.push(new InfoItem('Property Taxes', household.propertyTaxes));
+
+    insuranceData.push(new InfoItem('Insurance Company', household.insuranceCompany));
+    insuranceData.push(new InfoItem('Policy Number', household.policyNumber));
+
+    self.basicInformation = [financialData, insuranceData];
   }
 }());
