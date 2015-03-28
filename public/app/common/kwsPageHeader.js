754b763d17d9fd12dd96492a67b950175f399ec3
Ken Sodemann <ken.sodemann@gmail.com>
2015-03-28 17:23:59 -0500

    Generate a default edit template in the header

--- public/app/common/kwsPageHeader.js
+++ public/app/common/kwsPageHeader.js
@@ -18,17 +18,37 @@
   }
 
   function link(scope) {
-    generateTemplates(scope.kwsLines);
+    createDefaultTemplates(scope.kwsLines);
   }
 
-  function generateTemplates(lines) {
-    lines.forEach(function(item) {
-      if (!item.template) {
-        if (!item.columnName){
-          throw new Error("Must have a template or a column name");
-        }
-        item.template = "{{kwsModel." + item.columnName + "}}";
+  function createDefaultTemplates(lines) {
+    lines.forEach(function(line) {
+      if (isVisibleInViewMode(line) && !line.template) {
+        createViewModeTemplate(line);
+      }
+      if (isVisibleInEditMode(line) && !line.editTemplate) {
+        createEditModeTemplate(line);
       }
     });
+
+    function isVisibleInViewMode(line) {
+      return (line.modes && line.modes.indexOf('V') > -1);
+    }
+
+    function isVisibleInEditMode(line) {
+      return (line.modes && line.modes.indexOf('E') > -1);
+    }
+
+    function createViewModeTemplate(line) {
+      if (!line.columnName) {
+        throw new Error("Must have a template or a column name");
+      }
+      line.template = "{{kwsModel." + line.columnName + "}}";
+    }
+
+    function createEditModeTemplate(line){
+      var cn = line.columnName;
+      line.editTemplate = '<input class="form-control" name="' + cn + '" ng-model="kwsModel[\'' + cn + '\']">';
+    }
   }
 }());
--- test/client/common/kwsPageHeaderSpec.js
+++ test/client/common/kwsPageHeaderSpec.js
@@ -48,17 +48,23 @@
       });
     });
 
-    describe('Template', function() {
+    describe('View Mode Template', function() {
       beforeEach(function() {
         scope.myModel = {
           firstName: 'Peter',
           lastName: 'Piper',
-          address: '123 South Main Street'
+          address: '123 South Main Street',
+          city: 'Hamelin'
         };
         scope.lines = [{
-          columnName: 'address'
+          columnName: 'address',
+          modes: 'EV'
         }, {
-          template: '{{kwsModel.lastName}}, {{kwsModel.firstName}}'
+          template: '{{kwsModel.lastName}}, {{kwsModel.firstName}}',
+          modes: 'V'
+        }, {
+          columnName: 'city',
+          modes: 'E'
         }];
         el = angular.element('<kws-page-header kws-lines="lines" kws-model="myModel"></kws-page-header>');
         compile(el)(scope);
@@ -72,16 +78,57 @@
       it('is set to the column value for nodes associated with a column', function() {
         expect(scope.lines[1].template).to.equal('{{kwsModel.lastName}}, {{kwsModel.firstName}}');
       });
+
+      it('is not set if the line is not used in View mode', function() {
+        expect(scope.lines[2].template).to.be.undefined;
+      });
     });
 
-    describe('Bad Lines', function(){
-      it('raises an error ', function(done){
-        scope.lines = [{value: 'this is bogus'}];
+    describe('Edit Template', function() {
+      beforeEach(function() {
+        scope.myModel = {
+          firstName: 'Peter',
+          lastName: 'Piper',
+          address: '123 South Main Street',
+          city: 'Hamelin'
+        };
+        scope.lines = [{
+          columnName: 'address',
+          modes: 'EV'
+        }, {
+          editTemplate: '{{kwsModel.lastName}}, {{kwsModel.firstName}}',
+          modes: 'E'
+        }, {
+          columnName: 'city',
+          modes: 'V'
+        }];
+        el = angular.element('<kws-page-header kws-lines="lines" kws-model="myModel"></kws-page-header>');
+        compile(el)(scope);
+        scope.$digest();
+      });
+
+      it('is generated if it is not specified', function() {
+        expect(scope.lines[0].editTemplate).to.equal(
+           '<input class="form-control" name="address" ng-model="kwsModel[\'address\']">');
+      });
+
+      it('is set to the column value for nodes associated with a column', function() {
+        expect(scope.lines[1].editTemplate).to.equal('{{kwsModel.lastName}}, {{kwsModel.firstName}}');
+      });
+
+      it('is not set if the line is not used in Edit mode', function() {
+        expect(scope.lines[2].editTemplate).to.be.undefined;
+      });
+    });
+
+    describe('Bad Lines', function() {
+      it('raises an error ', function(done) {
+        scope.lines = [{value: 'this is bogus', modes: 'V'}];
         el = angular.element('<kws-page-header kws-lines="lines"></kws-page-header>');
-        try{
+        try {
           compile(el)(scope);
           scope.$digest();
-        } catch(err){
+        } catch (err) {
           expect(err.message).to.equal('Must have a template or a column name');
           done();
         }
