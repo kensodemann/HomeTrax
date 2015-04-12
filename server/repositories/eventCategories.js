a910dfa4c42d9f1619efe00a9b250b2cb3e69d0c
Ken Sodemann <ken.sodemann@gmail.com>
2015-04-12 07:25:54 -0500

    Use RepositoryBase for eventCategories.

--- server/repositories/eventCategories.js
+++ server/repositories/eventCategories.js
@@ -1,23 +1,14 @@
 'use strict';
 
 var db = require('../config/database');
-var ObjectId = require("mongojs").ObjectId;
+var RepositoryBase = require("./RepositoryBase");
+var util = require("util");
 
-module.exports.get = function(req, res) {
-  db.eventCategories.find(function(err, cats) {
-    res.send(cats);
-  });
-};
+function EventCategories(){
+  RepositoryBase.call(this);
+  this.collection = db.eventCategories;
+}
 
-module.exports.save = function(req, res) {
-  var cat = req.body;
-  var status = 201;
-  if (req.params && req.params.id) {
-    cat._id = new ObjectId(req.params.id);
-    status = 200;
-  }
-  db.eventCategories.save(req.body, function(err, cat) {
-    res.status(status);
-    res.send(cat);
-  });
-};
+util.inherits(EventCategories, RepositoryBase);
+
+module.exports = new EventCategories();
