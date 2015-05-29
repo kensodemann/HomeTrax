d5cf677d8c0d0751ce955679239a8c54b01060a6
Ken Sodemann <ken.sodemann@penta.com>
2015-05-29 06:25:13 -0500

    Add household capabilities to entity resource.

--- public/app/common/models/Entity.js
+++ public/app/common/models/Entity.js
@@ -1,11 +1,61 @@
 (function() {
   'use strict';
-  
+
   angular.module('app.core').factory('Entity', Entity);
-  
-  function Entity($resource){
-      return $resource('/api/entities/:id', {
+
+  function Entity($resource) {
+    var millisecondsPerMinute = 60000;
+
+    return $resource('/api/entities/:id', {
       id: "@_id"
+    }, {
+      query: {
+        method: 'GET',
+        isArray: true,
+        transformResponse: transformArrayResponse
+      },
+      save: {
+        method: 'POST',
+        transformResponse: transformResponse,
+        transformRequest: transformRequest
+      }
     });
+
+    function transformArrayResponse(data) {
+      var resp = angular.fromJson(data);
+      resp.forEach(function(item) {
+        if (item.purchaseDate) {
+          var d = new Date(item.purchaseDate);
+          item.purchaseDate = adjustDateForTimezone(d);
+        }
+      });
+      return resp;
+    }
+
+    function transformResponse(data) {
+      var resp = angular.fromJson(data);
+      if (resp.purchaseDate) {
+        var d = new Date(resp.purchaseDate);
+        resp.purchaseDate = adjustDateForTimezone(d);
+      }
+      return resp;
+    }
+
+    function transformRequest(data) {
+      if (data.purchaseDate) {
+        data.purchaseDate = removeTimezoneOffset(data.purchaseDate);
+      }
+      return angular.toJson(data);
+    }
+
+    function adjustDateForTimezone(d) {
+      var minutesFromUTC = d.getTimezoneOffset();
+      return new Date(d.getTime() + (minutesFromUTC * millisecondsPerMinute));
+    }
+
+    function removeTimezoneOffset(d) {
+      var minutesFromUTC = d.getTimezoneOffset();
+      return new Date(d.getTime() - (minutesFromUTC * millisecondsPerMinute));
+    }
   }
 })();
--- test/client/common/models/EntitySpec.js
+++ test/client/common/models/EntitySpec.js
@@ -25,8 +25,13 @@
       }, {
         _id: 3,
         name: 'Jack',
-        testTag: 314159,
+        testTag: 320,
         purchaseDate: '2014-07-28T00:00:00.000Z',
+        entityType: 'household'
+      }, {
+        _id: 4,
+        name: 'Jack',
+        testTag: 314159,
         entityType: 'household'
       }];
     });
@@ -51,10 +56,18 @@
       });
 
       it('gets the data', function() {
-        expect(res.length).to.equal(3);
+        expect(res.length).to.equal(4);
         expect(res[0].testTag).to.equal(42);
         expect(res[1].testTag).to.equal(73);
-        expect(res[2].testTag).to.equal(314159);
+        expect(res[2].testTag).to.equal(320);
+        expect(res[3].testTag).to.equal(314159);
+      });
+
+      it('Transforms purchase dates for the time zone', function() {
+        expect(res[0].purchaseDate).to.deep.equal(new Date(2013, 2, 3));
+        expect(res[1].purchaseDate).to.be.undefined;
+        expect(res[2].purchaseDate).to.deep.equal(new Date(2014, 6, 28));
+        expect(res[3].purchaseDate).to.be.undefined;
       });
     });
 
@@ -71,6 +84,37 @@
         httpBackend.expectPOST('/api/entities/2').respond(res[1]);
         res[1].$save();
         httpBackend.flush();
+      });
+
+      it('Adjusts the purchase date for TZ', function() {
+        res[0].purchaseDate = new Date(2011, 6, 15);
+        httpBackend.expectPOST('/api/entities/1', {
+          _id: 1,
+          name: 'Fred',
+          testTag: 42,
+          purchaseDate: '2011-07-15T00:00:00.000Z',
+          entityType: 'household'
+        }).respond({
+          _id: 1,
+          name: 'Fred',
+          testTag: 42,
+          purchaseDate: '2011-07-15T00:00:00.000Z'
+        });
+        res[0].$save();
+        httpBackend.flush();
+      });
+
+      it('Adjusts the purchase date in the response', function() {
+        httpBackend.expectPOST('/api/entities/1').respond({
+          _id: 1,
+          name: 'Fred',
+          testTag: 42,
+          purchaseDate: '2011-07-15T00:00:00.000Z',
+          entityType: 'household'
+        });
+        res[0].$save();
+        httpBackend.flush();
+        expect(res[0].purchaseDate).to.deep.equal(new Date(2011, 6, 15));
       });
     });
   });
