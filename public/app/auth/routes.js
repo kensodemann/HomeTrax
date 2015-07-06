239e5e12dee8b4bda494727732bee840b16567ad
Ken Sodemann <ken.sodemann@gmail.com>
2015-07-05 20:23:31 -0500

    Move public to www

--- public/app/auth/routes.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  'use strict';
-
-  angular.module('app.auth').constant('authRoutes', [{
-    path: '/login',
-    templateUrl: 'app/auth/templates/login.html',
-    controller: 'loginCtrl'
-  }]);
-})();
--- public/app/common/core/config.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-
-  angular.module('app.core').constant('config', {
-    dataService: 'http://localhost:8080'
-  });
-}());
