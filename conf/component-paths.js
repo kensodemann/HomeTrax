fb3e00635572519352996c626e9507ad8d2fa8d3
Ken Sodemann <ken.sodemann@penta.com>
2015-09-20 12:20:13 -0500

    chore(build): modify the build to use a source and dest directory

--- conf/component-paths.js
+++ conf/component-paths.js
@@ -1,62 +1,119 @@
 /*jshint node: true */
 
 module.exports = {
-  bower_components: 'bower_components',
-  scss: 'scss/manifests/penta.app.scss',
-  css: 'css',
-  test: 'test',
-  src: 'src',
+  css: {
+    angularMotion: {
+      dev: 'bower_components/angular-motion/dist/angular-motion.css',
+      release: 'bower_components/angular-motion/dist/angular-motion.min.css'
+    },
+    bootstrapAdditions: {
+      dev: 'bower_components/bootstrap-additions/dist/bootstrap-additions.css',
+      release: 'bower_components/bootstrap-additions/dist/bootstrap-additions.min.css',
+    },
+    fullcalendar: {
+      dev: 'bower_components/fullcalendar/dist/fullcalendar.css',
+      release: 'bower_components/fullcalendar/dist/fullcalendar.min.css'
+    }
+  },
   lib: {
     jquery: {
       dev: 'bower_components/jquery/dist/jquery.js',
       release: 'bower_components/jquery/dist/jquery.min.js'
-    },
+    }
+    ,
     angular: {
       dev: 'bower_components/angular/angular.js',
       release: 'bower_components/angular/angular.min.js'
-    },
-    angularBootstrap: {
-      dev: 'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
-      release: 'bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js'
-    },
-    angularUiRouter: {
-      dev: 'bower_components/angular-ui-router/release/angular-ui-router.js',
-      release: 'bower_components/angular-ui-router/release/angular-ui-router.min.js'
-    },
+    }
+    ,
     bootstrap: {
-      dev: 'bower_components/bootstrap/bootstrap.js',
-      release: 'bower_components/bootstrap/bootstrap.min.js'
-    },
+      dev: 'bower_components/bootstrap/dist/js/bootstrap.js',
+      release: 'bower_components/bootstrap/dist/js/bootstrap.min.js'
+    }
+    ,
+    angularAnimate: {
+      dev: 'bower_components/angular-animate/angular-animate.js',
+      release: 'bower_components/angular-animate/angular-animate.js'
+    }
+    ,
+    angularLocalStorage: {
+      dev: 'bower_components/angular-local-storage/dist/angular-local-storage.js',
+      release: 'bower_components/angular-local-storage/dist/angular-local-storage.min.js'
+    }
+    ,
     angularMessages: {
       dev: 'bower_components/angular-messages/angular-messages.js',
       release: 'bower_components/angular-messages/angular-messages.min.js'
-    },
+    }
+    ,
     angularResource: {
       dev: 'bower_components/angular-resource/angular-resource.js',
       release: 'bower_components/angular-resource/angular-resource.min.js'
-    },
+    }
+    ,
+    angularRoute: {
+      dev: 'bower_components/angular-route/angular-route.js',
+      release: 'bower_components/angular-route/angular-route.min.js'
+    }
+    ,
+    angularStrap: {
+      dev: 'bower_components/angular-strap/dist/angular-strap.js',
+      release: 'bower_components/angular-strap/dist/angular-strap.min.js'
+    }
+    ,
+    angularStrapTpl: {
+      dev: 'bower_components/angular-strap/dist/angular-strap.tpl.js',
+      release: 'bower_components/angular-strap/dist/angular-strap.tpl.min.js'
+    }
+    ,
+    angularTypeahead: {
+      dev: 'bower_components/angular-typeahead/angular-typeahead.js',
+      release: 'bower_components/angular-typeahead/angular-typeahead.min.js'
+    }
+    ,
+    angularUiCalendar: {
+      dev: 'bower_components/angular-ui-calendar/src/calendar.js',
+      release: 'bower_components/angular-ui-calendar/src/calendar.js'
+    }
+    ,
     angularMocks: {
       dev: 'bower_components/angular-mocks/angular-mocks.js'
-    },
-    angularUuidService: {
-      dev: 'bower_components/angular-uuid-service/angular-uuid-service.js',
-      release: 'bower_components/angular-uuid-service/angular-uuid-service.min.js'
-    },
-    moments: {
-      dev: 'bower_components/moment/min/moment-with-locales.js',
-      release: 'bower_components/moment/min/moment-with-locales.min.js'
-    },
+    }
+    ,
+    bloodhound: {
+      dev: 'bower_components/typeahead.js/dist/bloodhound.js',
+      release: 'bower_components/typeahead.js/dist/bloodhound.min.js'
+    }
+    ,
+    fullCalendar: {
+      dev: 'bower_components/fullcalendar/dist/fullcalendar.js',
+      release: 'bower_components/fullcalendar/dist/fullcalendar.min.js'
+    }
+    ,
+    moment: {
+      dev: 'bower_components/moment/moment.js',
+      release: 'bower_components/moment/min/moment.min.js'
+    }
+    ,
     toastr: {
       dev: 'bower_components/toastr/toastr.js',
       release: 'bower_components/toastr/toastr.min.js'
-    },
+    }
+    ,
+    typeahead: {
+      dev: 'bower_components/typeahead.js/dist/typeahead.jquery.js',
+      release: 'bower_components/typeahead.js/dist/typeahead.jquery.min.js'
+    }
+    ,
     underscore: {
       dev: 'bower_components/underscore/underscore.js',
       release: 'bower_components/underscore/underscore-min.js'
     }
-  },
+  }
+  ,
   fonts: {
-    fontAwesome: 'bower_components/font-awesome/fonts',
+    fontAwesome: 'bower_components/fontawesome/fonts',
     bootstrap: 'bower_components/bootstrap-sass/assets/fonts'
   }
-};
+}
+;
