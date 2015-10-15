aa75a449fefcf440f64b48c514179e59a6f0e36e
Ken Sodemann <ken.sodemann@penta.com>
2015-10-15 08:25:31 -0500

    feat(timesheet): layout the current timesheet view

--- src/app/timesheets/current/currentTimesheetController.js
+++ src/app/timesheets/current/currentTimesheetController.js
@@ -12,5 +12,16 @@
     });
 
   function CurrentTimesheetController() {
+    var controller = this;
+
+    controller.dates = [
+      new Date(2015, 9, 18),
+      new Date(2015, 9, 19),
+      new Date(2015, 9, 20),
+      new Date(2015, 9, 21),
+      new Date(2015, 9, 22),
+      new Date(2015, 9, 23),
+      new Date(2015, 9, 24)
+    ];
   }
 }());
