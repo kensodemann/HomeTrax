14e965e9b33a7cdd61b706457d4d22ae047693e8
Ken Sodemann <ken.sodemann@gmail.com>
2015-11-30 15:08:09 -0600

    chore(release): update version information

--- src/app/common/resources/ClientVersions.js
+++ src/app/common/resources/ClientVersions.js
@@ -7,8 +7,16 @@
       name: 'Narse (2.0.1)',
       releaseDate: '2015-11-30',
       description: 'Seperate the versions and improve the UI.',
-      features: ['Seperate Client and Server Versions'],
-      bugFixes: []
+      features: [
+        'Version the client and the server seperately',
+        'Display the stage in the timer on the current timesheet view',
+        'Show the JIRA and SBVB task ID in the project drop-down list',
+        'Hide the JIRA and SBVB task IDs if they do not exist',
+        'Refresh the displayed elapsed time as a timer runs'
+      ],
+      bugFixes: [
+        'Fixed issue with total time not refreshing when a timer was stopped'
+      ]
     }, {
       id: '2.0.0',
       name: 'BETA (2.0.0)',
