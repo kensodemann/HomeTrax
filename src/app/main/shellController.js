07bc424866d436592b99f73c6d564aeb6991783c
Ken Sodemann <ken.sodemann@gmail.com>
2015-12-09 23:54:16 +0000

    fix(auth): remove bad code copied from elsewhere

--- src/app/main/shellController.js
+++ src/app/main/shellController.js
@@ -9,10 +9,5 @@
     $scope.$on(AuthEvents.notAuthenticated, function() {
       $state.go('app.login');
     });
-
-    // TODO: Change this after getting the errors from the server sorted out...
-    $scope.$on(AuthEvents.notAuthorized, function() {
-      $state.path('app.login');
-    });
   }
 }());
